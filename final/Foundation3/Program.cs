4bac0dfca8b01b9072cd62023f7fe1ba4291da52
byu-alumni-kpalmer <pal23006@byui.edu>
2024-02-21 19:49:03 -0800

    final

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,25 @@
 using System;
 
-internal class Program
+class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address = new Address("1645 E. Thomas RD", "Phoenix", "AZ", "85016");
+        Lecture lecture = new Lecture("Palmer Keith", "BYU-Student", "2024-10-25", "11:00 AM", address, "Ermilus", 32);
+        Reception reception = new ("Networking Mixer", "Network with professionals", "2024-02-20", "6:00 PM", address, "pal23006@byui.edu");
+        OutdoorGathering outdoorGathering = new OutdoorGathering("Rosevelt Row", "First Fridays", "2024-11-26", "1:00 PM", address);
+
+        Event[] events = { lecture, reception, outdoorGathering };
+
+        foreach (Event e in events)
+        {
+            Console.WriteLine("Standard Details:");
+            Console.WriteLine(e.GetStandardDetails());
+
+            Console.WriteLine("\nShort Description:");
+            Console.WriteLine(e.GetShortDescription());
+
+            Console.WriteLine();
+        }
     }
 }
